9b43236e032279c75aa510ebdf72fb60815b7e67
Simple <248200851@qq.com>
2019-11-20 19:06:42 +0800

    refactor(env): MARIADBPWD -> DB_PASSWORD

--- src/app/config/prod.config.ts
+++ src/app/config/prod.config.ts
@@ -1,10 +1,10 @@
-import { MARIADBPWD } from './variate';
+import { DB_PASSWORD } from './variate';
 // 正式版
 export default {
   mariadb: {
     host: 'xxx.xxx.xxx.xxx', // 数据库地址
     user: 'root', // 数据库账号
-    password: MARIADBPWD, // 数据库密码
+    password: DB_PASSWORD, // 数据库密码
     database: 'koa-ts-full-template-db' // 数据库名
   }
 };
